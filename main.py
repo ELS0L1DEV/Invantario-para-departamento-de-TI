d5e2ad61ad7d2780282268f4565896cdb76d1bfe
Esteban Maya <esteban.may.mtz@gmail.com>
2026-03-17 23:26:07 -0600

    Importando metodo de login a main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 # main.py
-from src.ui.login import LoginApp
+from src.ui.login import intentar_login # O como se llame la función principal en tu login.py
+import customtkinter as ctk
 
+# Esto asegura que el código solo corra si ejecutas main.py directamente
 if __name__ == "__main__":
-    app = LoginApp()
-    app.mainloop()
+    # Aquí iría el código para inicializar tu app, por ejemplo:
+    # app = ctk.CTk()
+    # Aquí llamas a la interfaz de tu login
+    pass
